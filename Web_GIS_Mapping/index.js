fe30e6807bd827b08daa57f3bb1e3641877c7034
Kusasalethu Sithole <skusasalethu@gmail.com>
2020-01-10 11:58:26 +0200

    Add Map Layers Switching Controls

--- Web_GIS_Mapping/index.js
+++ Web_GIS_Mapping/index.js
@@ -1,30 +1,23 @@
 window.onload = init;
 
-//Map Controls
-//Standard Controls
-const mapOverviewControl = new ol.control.OverviewMap({
+//Map View
+function init(){
+  //Standard Map Controls
+  const mapOverviewControl = new ol.control.OverviewMap({
     collapsed: false,
     layers: [
         new ol.layer.Tile({
           source: new ol.source.OSM()
         })
       ]
-});
-const mapFullScreenControl = new ol.control.FullScreen();
-const scaleLineControl = new ol.control.ScaleLine({
+  });
+  const mapFullScreenControl = new ol.control.FullScreen();
+  const scaleLineControl = new ol.control.ScaleLine({
   target: document.getElementById('scaling')
-});
-const zoomSliderControl = new ol.control.ZoomSlider();
-const zoomToExtentControl = new ol.control.ZoomToExtent();
+  });
+  const zoomSliderControl = new ol.control.ZoomSlider();
+  const zoomToExtentControl = new ol.control.ZoomToExtent();
 
-//Base Map Switch Controls
-
-
-//Layer Switch Controls
-
-
-//Map View
-function init(){
   // Map Object
   const map = new ol.Map({
     target: 'map',
@@ -44,36 +37,55 @@ function init(){
   // Base Map LayerGroup 
   //Standard OSM
   const standardOSM = new ol.layer.Tile({
-    source: new ol.source.OSM(),
+    source: new ol.source.OSM({
+      attributions: 'Credit to <a href=https://www.openstreetmap.org>© OSM</a>'
+    }),
     zIndex: 0,
-    visible:true
+    visible:true,
+    title: 'standardOSM'
   });
   //OSM Humanitarian
   const humanitarianOSM = new ol.layer.Tile({
     source: new ol.source.OSM({
-      url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+      url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+      attributions: 'Credit to <a href=https://www.openstreetmap.org>© OSM</a>'
     }),
     zIndex: 0,
-    visible:false
+    visible:false,
+    title: 'humanitarianOSM'
   });
   // Bing Maps
   const bingMaps = new ol.layer.Tile({
     source: new ol.source.BingMaps({
       key: "AvYLDCDCkIuh75GSuBR2NlcE-gHGtpuHwbFTMPBzlD__tKA8UTe5bHpIoO7Kmyh9",
-      imagerySet: "Aerial"
+      imagerySet: "Aerial",
+      attributions: 'Credit to <a href=https://www.bing.com/maps>© Bing Maps</a>'
     }),
     zIndex: 0,
-    visible:false
+    visible:false,
+    title: 'bingMaps'
   });
 
-  const baseMaps = new ol.layer.Group({
+  const baseMapsGroup = new ol.layer.Group({
     layers: [
       standardOSM,
       humanitarianOSM,
       bingMaps
     ]
   });
-  map.addLayer(baseMaps);
+  map.addLayer(baseMapsGroup);
+
+  //Base Map Switch Controls
+  const baseMapsElements = document.querySelectorAll('.base-maps > input');
+  for (let baseMapsElement of baseMapsElements){
+  baseMapsElement.addEventListener('change', function(){
+    let baseMapsElementValue = this.value;
+    baseMapsGroup.getLayers().forEach(function(element, index, array){
+      let baseMapTitle = element.get('title');
+      element.setVisible(baseMapTitle === baseMapsElementValue);
+    })
+  })
+  }
 
   // Layer LayerGroup
   //NOAA WMS Layer
@@ -89,7 +101,8 @@ function init(){
     }),
     zIndex: 1,
     opacity: 0.5,
-    visible:true
+    visible:true,
+    title: 'NOAALayer'
   });
 
   // sa provinces KML
@@ -100,14 +113,33 @@ function init(){
     }),
     zIndex: 2,
     opacity: 1,
-    visible:true   
+    visible:true,
+    title: 'examplesKML'  
   });
 
-  const overLayers = new ol.layer.Group({
+  const layersGroup = new ol.layer.Group({
     layers: [
       NOAALayer,
       examplesKML
     ]
   });
-  map.addLayer(overLayers);
+  map.addLayer(layersGroup);
+
+  //Layer Switch Controls
+  const layerElements = document.querySelectorAll('.layers > input');
+  for (let layerElement of layerElements){
+  layerElement.addEventListener('change', function(){
+    let layerElementValue = this.value;
+    let targetLayer;
+
+    layersGroup.getLayers().forEach(function(element, index, array){
+      let layerTitle = element.get('title');
+      if(layerElementValue === layerTitle){
+        targetLayer = element;
+      }
+    })
+    this.checked ? targetLayer.setVisible(true) : targetLayer.setVisible(false)
+  })
+  }
+
 }
